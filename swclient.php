dfb91179ac831ee24ee3178729af249931dc4c6a
Chang Long Xu <xcl_rockman@qq.com>
2015-06-10 14:35:51 +0800

    init

---
+++ swclient.php
@@ -0,0 +1,210 @@
+<?php
+
+
+/**
+ * Class DoraRPCClient
+ */
+class DoraRPCClient
+{
+
+    const SW_SYNC_SINGLE = 'SSS';
+    const SW_RSYNC_SINGLE = 'SRS';
+
+    const SW_SYNC_MULTI = 'SSM';
+    const SW_RSYNC_MULTI = 'SRM';
+
+    private static $client;
+
+    private $ip;
+    private $port;
+
+    function __construct($ip = "127.0.0.1", $port = 9567)
+    {
+        if (!self::$client) {
+            self::$client = new swoole_client(SWOOLE_SOCK_TCP | SWOOLE_KEEP);
+            self::$client->set(array(
+                'open_length_check' => 1,
+                'package_length_type' => 'N',
+                'package_length_offset' => 0,
+                'package_body_offset' => 4,
+                'package_max_length' => 1024 * 1024 * 2,
+                'open_tcp_nodelay' => 1,
+            ));
+        }
+
+        $this->ip = $ip;
+        $this->port = $port;
+
+        $ret = $this->connect();
+
+        if ($ret["code"] != 0) {
+            return $ret;
+        } else {
+            return $this->packFormat();
+        }
+    }
+
+    private function connect()
+    {
+        if (!self::$client->connect($this->ip, $this->port, 3.0)) {
+            $errorcode = self::$client->errCode;
+            if ($errorcode == 0) {
+                $msg = "connect fail.check host dns.";
+                $errorcode = -1;
+                $packet = $this->packFormat($msg, $errorcode);
+            } else {
+                $msg = socket_strerror($errorcode);
+                $packet = $this->packFormat($msg, $errorcode);
+            }
+
+            return $packet;
+        }
+        #success
+        return $this->packFormat();
+    }
+
+    /**
+     * 单api请求
+     * @param string $api api地址
+     * @param array $param 参数
+     * @param bool $sync 阻塞等待结果
+     * @return mixed 返回单个请求结果
+     */
+    public function singleAPI($name, $param, $sync = true)
+    {
+        $guid = md5(uniqid() . microtime(true) . rand(1, 1000000));
+        $packet = array(
+            'guid' => $guid,
+            'api' => array(
+                "one" => array(
+                    'name' => $name,
+                    'param' => $param,
+                )
+            ),
+        );
+
+        if ($sync) {
+            $packet["type"] = self::SW_SYNC_SINGLE;
+        } else {
+            $packet["type"] = self::SW_RSYNC_SINGLE;
+        }
+
+        $sendData = $this->packEncode($packet);
+
+        $ret = self::$client->send($sendData);
+        if (!$ret) {
+            $this->connect();
+            $ret = self::$client->send($sendData);
+        }
+
+        if (!$ret) {
+            $errorcode = self::$client->errCode;
+            if ($errorcode == 0) {
+                $msg = "connect fail.check host dns.";
+                $errorcode = -1;
+                $packet = $this->packFormat($msg, $errorcode);
+            } else {
+                $msg = socket_strerror($errorcode);
+                $packet = $this->packFormat($msg, $errorcode);
+            }
+
+            return $packet;
+        }
+
+        $result = self::$client->recv();
+        $result = $this->packDecode($result);
+
+        //todo:guid verify
+        return $result;
+    }
+
+    /**
+     * 并发请求api，使用方法如
+     * $params = array(
+     *  "api_1117"=>array("name"=>"apiname1",“param”=>array("id"=>1117)),
+     *  "api_2"=>array("name"=>"apiname2","param"=>array("id"=>2)),
+     * )
+     * @param array $params 提交参数 请指定key好方便区分对应结果，注意考虑到硬件资源有限并发请求不要超过50个
+     * @param bool $sync 阻塞等待所有结果
+     * @return mixed 返回指定key结果
+     * @throws exception 并发超过50 会报错
+     */
+    public function multiAPI($params, $sync = true)
+    {
+
+        $this->connect();
+        $guid = md5(uniqid() . microtime(true) . rand(1, 1000000));
+        $packet = array(
+            'guid' => $guid,
+            'api' => $params,
+        );
+
+        if ($sync) {
+            $packet["type"] = self::SW_SYNC_MULTI;
+        } else {
+            $packet["type"] = self::SW_RSYNC_MULTI;
+        }
+
+        $sendData = $this->packEncode($packet);
+
+        $ret = self::$client->send($sendData);
+
+        if (!$ret) {
+            $this->connect();
+            $ret = self::$client->send($sendData);
+        }
+
+        if (!$ret) {
+            $errorcode = self::$client->errCode;
+            if ($errorcode == 0) {
+                $msg = "connect fail.check host dns.";
+                $errorcode = -1;
+                $packet = $this->packFormat($msg, $errorcode);
+            } else {
+                $msg = socket_strerror($errorcode);
+                $packet = $this->packFormat($msg, $errorcode);
+            }
+            return $packet;
+        }
+
+        $result = self::$client->recv();
+
+        $result = $this->packDecode($result);
+        //todo:guid verify
+        return $result;
+    }
+
+    private function packFormat($msg = "OK", $code = 0, $data = array())
+    {
+        $pack = array(
+            "code" => $code,
+            "msg" => $msg,
+            "data" => $data,
+        );
+        return $pack;
+    }
+
+    private function packEncode($data)
+    {
+        $sendStr = serialize($data);
+        $sendStr = pack('N', strlen($sendStr)) . $sendStr;
+        return $sendStr;
+    }
+
+    private function packDecode($str)
+    {
+        $header = substr($str, 0, 4);
+        $result = substr($str, 4);
+
+        $len = unpack("Nlen", $header);
+        $result = unserialize($result);
+
+        return $this->packFormat("OK", 0, $result);
+    }
+
+
+    function __destruct()
+    {
+
+    }
+}
---
+++ swserver.php
@@ -0,0 +1,278 @@
+<?php
+/**
+ * Class DoraRPCServer
+ */
+class DoraRPCServer
+{
+    const SW_SYNC_SINGLE = 'SSS';
+    const SW_RSYNC_SINGLE = 'SRS';
+
+    const SW_SYNC_MULTI = 'SSM';
+    const SW_RSYNC_MULTI = 'SRM';
+
+    private $server = null;
+    private $taskInfo = array();
+
+    final function __construct($ip = "0.0.0.0", $port = 9567)
+    {
+        $this->server = new swoole_server($ip, $port);
+        $config = array(
+            'open_length_check' => 1,
+            'dispatch_mode' => 3,
+            'package_length_type' => 'N',
+            'package_length_offset' => 0,
+            'package_body_offset' => 4,
+            'package_max_length' => 1024 * 1024 * 2,
+            'buffer_output_size' => 1024 * 1024 * 3,
+            'pipe_buffer_size' => 1024 * 1024 * 32,
+            'open_tcp_nodelay' => 1,
+            'heartbeat_check_interval' => 5,
+            'heartbeat_idle_time' => 10,
+
+            'reactor_num' => 32,
+            'worker_num' => 40,
+            'task_worker_num' => 20,
+
+            'max_request' => 0, #必须设置为0否则并发任务容易丢
+            'task_max_request' => 4000,
+
+            'backlog' => 2000,
+            'log_file' => '/tmp/sw_server.log',
+            'task_tmpdir' => '/tmp/swtasktmp/',
+
+            //'daemonize' => 1,
+        );
+        $this->server->set($config);
+
+        $this->server->on('connect', array($this, 'onConnect'));
+        $this->server->on('workerstart', array($this, 'onWorkerStart'));
+        $this->server->on('receive', array($this, 'onReceive'));
+        $this->server->on('workererror', array($this, 'onWorkerError'));
+        $this->server->on('task', array($this, 'onTask'));
+        $this->server->on('close', array($this, 'onClose'));
+        $this->server->on('finish', array($this, 'onFinish'));
+
+        $this->server->start();
+    }
+
+    final function onConnect($serv, $fd)
+    {
+        $this->taskInfo[$fd] = array();
+    }
+
+    final function onWorkerStart($server, $worker_id)
+    {
+        swoole_set_process_name("swworker|{$worker_id}");
+    }
+
+    final function onReceive(swoole_server $serv, $fd, $from_id, $data)
+    {
+        $reqa = $this->packDecode($data);
+        #decode error
+        if ($reqa["code"] != 0) {
+            $req = $this->packEncode($reqa);
+            $serv->send($fd, $req);
+            return true;
+        } else {
+            $req = $reqa["data"];
+        }
+
+        #api not set
+        if (!is_array($req["api"]) && count($req["api"])) {
+            $pack = $this->packFormat("param api is empty", 100003);
+            $pack = $this->packEncode($pack);
+            $serv->send($fd, $pack);
+            return true;
+        }
+
+        $this->taskInfo[$fd] = $req;
+
+        $task = array(
+            "type" => $this->taskInfo[$fd]["type"],
+            "guid" => $this->taskInfo[$fd]["guid"],
+            "fd" => $fd,
+        );
+
+        echo "#{$serv->worker_id}>> received length=" . strlen($data) . "\n";
+
+        switch ($this->taskInfo[$fd]["type"]) {
+
+            case self::SW_SYNC_SINGLE:
+                $task["api"] = $this->taskInfo[$fd]["api"]["one"];
+                $taskid = $serv->task($task);
+
+                $this->taskInfo[$fd]["task"][$taskid] = "one";
+                return true;
+                break;
+            case self::SW_RSYNC_SINGLE:
+                $task["api"] = $this->taskInfo[$fd]["api"]["one"];
+                $serv->task($task);
+
+                $pack = $this->packFormat("已经成功投递", 100001);
+                $pack = $this->packEncode($pack);
+                $serv->send($fd, $pack);
+
+                unset($this->taskInfo[$fd]);
+                return true;
+
+                break;
+
+            case self::SW_SYNC_MULTI:
+                foreach ($req["api"] as $k => $v) {
+                    $task["api"] = $this->taskInfo[$fd]["api"][$k];
+                    $taskid = $serv->task($task);
+                    $this->taskInfo[$fd]["task"][$taskid] = $k;
+                }
+
+                return true;
+                break;
+            case self::SW_RSYNC_MULTI:
+                foreach ($req["api"] as $k => $v) {
+                    $task["api"] = $this->taskInfo[$fd]["api"][$k];
+                    $serv->task($task);
+                }
+                $pack = $this->packFormat("已经成功投递", 100001);
+                $pack = $this->packEncode($pack);
+
+                $serv->send($fd, $pack);
+                unset($this->taskInfo[$fd]);
+
+                return true;
+                break;
+            default:
+                $pack = $this->packFormat("未知类型任务", 100002);
+                $pack = $this->packEncode($pack);
+
+                $serv->send($fd, $pack);
+                unset($this->taskInfo[$fd]);
+                return true;
+        }
+
+        return true;
+    }
+
+    final function onTask($serv, $task_id, $from_id, $data)
+    {
+        //$data["result"] = array("yes" => "ok");
+        swoole_set_process_name("phptask|{$task_id}|" . $data["api"]["name"] . "");
+        try {
+            $data["result"] = $this->doWork($data);
+        } catch (Exception $e) {
+            $data["result"] = $this->packFormat($e->getMessage(), $e->getCode());
+            return $data;
+        }
+        return $data;
+    }
+
+    public function doWork($param)
+    {
+        return array("yes" => "ok");
+    }
+
+    //function onWorkerStart( $server,  $worker_id)
+    //{
+    //}
+
+    final function onWorkerError(swoole_server $serv, $worker_id, $worker_pid, $exit_code)
+    {
+        $this->log("WorkerError", array($this->taskInfo, $serv, $worker_id, $worker_pid, $exit_code));
+    }
+
+    private function log($type, $content, $file = "sw_error.log")
+    {
+        $result = date("Y-m-d H:i:s") . "|$type|" . json_encode($content) . "\r\n";
+        file_put_contents("/tmp/" . $file, $result, FILE_APPEND);
+    }
+
+    final function onFinish($serv, $task_id, $data)
+    {
+        $fd = $data["fd"];
+
+        if (!isset($this->taskInfo[$fd]) || !$data["result"]) {
+            unset($this->taskInfo[$fd]);
+            return true;
+        }
+
+        $key = $this->taskInfo[$fd]["task"][$task_id];
+        $this->taskInfo[$fd]["result"][$key] = $data["result"];
+
+        unset($this->taskInfo[$fd]["task"][$task_id]);
+
+        switch ($data["type"]) {
+
+            case self::SW_SYNC_SINGLE:
+                $packet = $this->packFormat("OK", 0, $data["result"]);
+                $packet = $this->packEncode($packet);
+                $serv->send($fd, $packet);
+                unset($this->taskInfo[$fd]);
+                return true;
+                break;
+
+            case self::SW_SYNC_MULTI:
+                if (count($this->taskInfo[$fd]["task"]) == 0) {
+                    $packet = $this->packFormat("OK", 0, $this->taskInfo[$fd]["result"]);
+                    $packet = $this->packEncode($packet);
+                    $serv->send($fd, $packet);
+                    unset($this->taskInfo[$fd]);
+                    return true;
+                } else {
+                    return true;
+                }
+                break;
+
+            default:
+                unset($this->taskInfo[$fd]);
+                return true;
+                break;
+        }
+
+    }
+
+    final function onClose(swoole_server $server, $fd, $from_id)
+    {
+        unset($this->taskInfo[$fd]);
+    }
+
+
+    private function packEncode($data)
+    {
+        $sendStr = serialize($data);
+        $sendStr = pack('N', strlen($sendStr)) . $sendStr;
+        return $sendStr;
+    }
+
+    private function packDecode($str)
+    {
+        $header = substr($str, 0, 4);
+        $result = substr($str, 4);
+
+        $len = unpack("Nlen", $header);
+
+        if ($len["len"] != strlen($result)) {
+            //结果长度不对
+            echo "error length...\n";
+            return $this->packFormat("包长度非法", 100007);
+        }
+        $result = unserialize($result);
+
+        return $this->packFormat("OK", 0, $result);
+    }
+
+    private function packFormat($msg = "OK", $code = 0, $data = array())
+    {
+        $pack = array(
+            "code" => $code,
+            "msg" => $msg,
+            "data" => $data,
+        );
+        return $pack;
+    }
+
+
+    final function __destruct()
+    {
+        $this->server->shutdown();
+    }
+
+
+}
