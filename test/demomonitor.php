435db9004d1aba1e9a53c3dda1054713423d91c3
Chang Long Xu <xcl_rockman@qq.com>
2016-06-29 10:25:36 +0800

    change file case for composer

--- test/demomonitor.php
+++ test/demomonitor.php
@@ -1,7 +1,7 @@
 <?php
-include "../src/doraconst.php";
-include "../src/packet.php";
-include "../src/monitor.php";
+include "../src/Doraconst.php";
+include "../src/Packet.php";
+include "../src/Monitor.php";
 
 $config = array(
     //redis for service discovery register
